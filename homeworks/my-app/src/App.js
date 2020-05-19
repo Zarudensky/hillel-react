6796b256fc7cfef5b1b5633e1505896fef68ed26
Stas Zarudensky <staszarudensky@Stass-MacBook-Pro.local>
2020-05-19 17:38:48 +0300

    Added first component

--- homeworks/my-app/src/App.js
+++ homeworks/my-app/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    const name = "Stas";
+    return (
+      <div className="main__container">
+        <p className="title">Hellow world!</p>
+        <img src={logo} className="react__logo" alt="logo" />
+        <p className="descr">My name is {name}</p>
+        <p className="descr">This is my first React app.</p>
+      </div>
+    )
+  }
 }
-
-export default App;
